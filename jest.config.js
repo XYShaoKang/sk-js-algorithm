ae0e678024a631f34afde467f066b7b15ff47819
XYShaoKang <38753204+XYShaoKang@users.noreply.github.com>
2021-10-10 02:10:19 +0800

    chore: fix jest config type

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+/** @type {import('@jest/types').Config.DefaultOptions} */
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest/setupTests.ts'],
   testEnvironment: 'node',
