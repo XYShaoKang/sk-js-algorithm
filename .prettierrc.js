8e74628b0293a023bab114fe501341838f74848a
XYShaoKang <38753204+XYShaoKang@users.noreply.github.com>
2022-01-06 17:00:46 +0800

    chore: add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import('prettier').Options}
+ */
+module.exports = {
+  trailingComma: `all`,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  endOfLine: `lf`,
+  printWidth: 120,
+  arrowParens: `avoid`,
+  proseWrap: 'preserve',
+}
